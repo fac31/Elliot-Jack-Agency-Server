8326de71c8373b70ba161f335be1c24f196e7867
Jack Parsons <jackparse14@hotmail.co.uk>
2024-04-01 21:26:56 +0100

    .gitignore

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.log(error))
 db.once('open', () => console.log('Connected to database'))
-
 app.use(express.json())
 
 const propertyData = require('./routes/propertyData')
